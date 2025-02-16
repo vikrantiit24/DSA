ed3a8f672f736dffaca850382743e543105d0bf4
vikrant bhan <149548297+vikrantiit24@users.noreply.github.com>
2025-02-16 14:30:31 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class assignment {
+    public static void main(String[] args) {
+        //factorial
+        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//         int fact =1;
+//         int i=1;
+//         while (i<=n ){
+//             fact*=i;
+//             i++;
+//         }
+//        System.out.println(fact);
+        //electricity bill
+//        System.out.println("emter the number of devixes");
+//        int num = in.nextInt();
+//        int total_units=0;
+//        int i =1;
+//        while(i<=num){
+//            System.out.println("Enter units consumed for reading " + i + ": ");
+//            int unit_consumed= in.nextInt();
+//            total_units+=unit_consumed;
+//            i++;
+//        }
+//        System.out.println(total_units);
+        //average of numbers
+//        System.out.println("enter numbers ");
+//        int number = in.nextInt();
+//        int i =1;
+//        int n =1;
+//        int sum=0;
+//        while(i<=number){
+//            System.out.println("enter number"+i);
+//            int num= in.nextInt();
+//             sum+=num;
+//            i++;
+//        }
+//        double average = sum/number;
+//        System.out.println("the average of these numbers is "+average);
+        //discout calculator
+        
+
+
+    }
+}
